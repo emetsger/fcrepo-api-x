3f8de1ac7d656b1d751f283b157549283014054b
Elliot Metsger <emetsger@jhu.edu>
2017-02-07 10:15:18 -0500

    Update accept mime type to something SHARE accepts

--- proof-of-concept/go-proxy/proxy.go
+++ proof-of-concept/go-proxy/proxy.go
@@ -159,6 +159,9 @@ func (t *redactingTransport) RoundTrip(req *http.Request) (*http.Response, error
 	log.Printf("Request url: %s", req.URL)
 	log.Printf("Request: %v\n", req)
 
+	req.Header.Del("Accept")
+	req.Header.Add("Accept", "application/vnd+api.json")
+
 	resp, err := t.delegate.RoundTrip(req)
 	if err != nil {
 		log.Panicf("Response error: %s", err)
